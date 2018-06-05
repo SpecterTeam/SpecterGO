cb93572f0bde55e55413beb4aa9226c88a2f6cf4
Fris <ilyesou.cherfaoui.18@gmail.com>
2018-06-05 10:44:33 -0700

    Updated HelpCommand.go

--- command/HelpCommand.go
+++ command/HelpCommand.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"github.com/SpecterTeam/SpecterGO/utils"
+	"strconv"
 )
 
 func HelpCommand() Command {
@@ -11,7 +12,41 @@ func HelpCommand() Command {
 func executeHelp(args []string){
 	logger := utils.Logger{}
 	logger.Info("Commands :")
-	for name,c := range GetCommandMap().Commands() {
-		logger.Info("/" + name + ": " + c.Description())
+	if len(args) == 0 {
+		for name, c := range GetCommandMap().Commands() {
+			logger.Info("/" + name + ": " + c.Description())
+		}
+	} else {
+		page,err := strconv.Atoi(args[0])
+		if err != nil {
+			utils.HandleError(err)
+		} else {
+			list := makeHelpList()
+			max := len(list)
+			if page > max {
+				for name,c := range list[0] {
+					logger.Info("/" + name + ": " + c.Description())
+				}
+			} else {
+				page--
+				for name,c := range list[page] {
+					logger.Info("/" + name + ": " + c.Description())
+				}
+			}
+		}
 	}
+}
+
+func makeHelpList() map[int]map[string]Command {
+	list := make(map[int]map[string]Command)
+	count,page := 0,0
+	for name,c := range GetCommandMap().Commands() {
+		if count == 5 {
+			page++
+		}
+		list[page][name] = c
+		count++
+	}
+
+	return list
 }
